1029d114edace558a38e683673ca94bcef26bc2c
jnewkirk <newkirk.jim@gmail.com>
2017-04-30 14:17:23 -0700

    Removed Adobe documentation header

--- collectionsetup.lrdevplugin/Info.lua
+++ collectionsetup.lrdevplugin/Info.lua
@@ -1,23 +1,3 @@
---[[----------------------------------------------------------------------------
-
-ADOBE SYSTEMS INCORPORATED
- Copyright 2007 Adobe Systems Incorporated
- All Rights Reserved.
-
-NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
-with the terms of the Adobe license agreement accompanying it. If you have received
-this file from a source other than Adobe, then your use, modification, or distribution
-of it requires the prior written permission of Adobe.
-
---------------------------------------------------------------------------------
-
-Info.lua
-Summary information for Hello World sample plug-in.
-
-Adds menu items to Lightroom.
-
-------------------------------------------------------------------------------]]
-
 return {
     LrPluginName          = "Collection Setup",
     LrToolkitIdentifier   = 'com.codeside.collectionsetup',
